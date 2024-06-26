f0093d17105e2f4e565043f4d803e56e3307bd3f
Fadil-Tao <hadadfadilah2354@gmail.com>
2024-06-26 12:39:08 +0800

    update navbar responsive and logo

--- src/lib/components/NavButton.svelte
+++ src/lib/components/NavButton.svelte
@@ -8,8 +8,8 @@
 	<div
 		class={`flex justify-center items-center border-plum mx-2 md:mx-6 hover:text-plum ${$page.url.pathname === `/${to}` ? ' text-plum border-b-2' : ''}`}
 	>
-		<a href={`/${to}`} class="flex justify-center items-start md:text-lg text-xs">
-			<div class="md:block hidden">hello</div>
+		<a href={`/${to}`} class="flex justify-center items-center md:items-start md:text-lg text-xs">
+			<div class="block"><slot /></div>
 			{title}
 		</a>
 	</div>
--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,15 +1,22 @@
 <script>
 	import NavButton from './NavButton.svelte';
+	import {IconHome,IconBook,IconHammer,IconBrandLinkedin,IconBrandX} from "@tabler/icons-svelte"
 </script>
 
 <nav class="flex justify-between bg-[#191724] rounded-xl p-3 ">
 	<div class="flex lg:justify-normal justify-center items-center">
 		<NavButton to={''} title={'Home'}>
-		<NavButton to={'projects'} title={'Projects'} />	
-		<NavButton to={'blog'} title={'Blog'}/>
+			<IconHome/>
+		</NavButton>
+		<NavButton to={'projects'} title={'Projects'}>
+			<IconHammer/>
+		</NavButton>	
+		<NavButton to={'blog'} title={'Blog'}>
+			<IconBook/>
+		</NavButton>
 	</div>
-	<div class="lg:flex hidden ">
-		<NavButton to={'music'} title={''} />
-		<NavButton to={'games'} title={''}/>
+	<div class="lg:flex  ">
+		<NavButton to={'music'} title={''}><IconBrandLinkedin/></NavButton>
+		<NavButton to={'games'} title={''}><IconBrandX/></NavButton>
 	</div>
 </nav>
