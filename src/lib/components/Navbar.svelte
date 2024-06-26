ac0afad48db910455169f4f51059fc37f588d7c7
Fadil-Tao <hadadfadilah2354@gmail.com>
2024-06-26 18:15:51 +0800

    fix the navbar responsiveness

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,22 +1,36 @@
 <script>
 	import NavButton from './NavButton.svelte';
-	import {IconHome,IconBook,IconHammer,IconBrandLinkedin,IconBrandGithub} from "@tabler/icons-svelte"
+	import {
+		IconHome,
+		IconBook,
+		IconHammer,
+		IconBrandLinkedin,
+		IconBrandGithub
+	} from '@tabler/icons-svelte';
 </script>
 
-<nav class="flex justify-between bg-[#191724] rounded-xl p-3 ">
+<nav class="flex justify-between bg-[#191724] rounded-xl p-3">
 	<div class="flex lg:justify-normal justify-center items-center">
 		<NavButton to={''} title={'Home'}>
-			<IconHome/>
+			<div class=" md:block hidden">
+				<IconHome />
+			</div>
 		</NavButton>
 		<NavButton to={'projects'} title={'Projects'}>
-			<IconHammer/>
-		</NavButton>	
+			<div class=" md:block hidden">
+				<IconHammer />
+			</div>
+		</NavButton>
 		<NavButton to={'blog'} title={'Blog'}>
-			<IconBook/>
+			<div class=" md:block hidden">
+				<IconBook />
+			</div>
 		</NavButton>
 	</div>
-	<div class="md:flex  ">
-		<NavButton to={'https://www.linkedin.com/in/hadad-fadilah234/'} title={''}><IconBrandLinkedin/></NavButton>
-		<NavButton to={'https://github.com/Fadil-Tao'} title={''}><IconBrandGithub/></NavButton>
+	<div class="md:flex">
+		<NavButton to={'https://www.linkedin.com/in/hadad-fadilah234/'} title={''}
+			> <IconBrandLinkedin /></NavButton
+		>
+		<NavButton to={'https://github.com/Fadil-Tao'} title={''}><IconBrandGithub /></NavButton>
 	</div>
 </nav>
